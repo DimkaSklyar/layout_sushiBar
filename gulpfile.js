cd0854006377d0667a99f431e870ae6f6628c1d8
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2020-02-09 16:23:45 +0300

    v.0.3.0

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('js', function() {
 			'src/libs/jquery/dist/jquery.min.js',
 			'src/js/common.js', // Always at the end
 			])
-		//.pipe(concat('scripts.min.js'))
+		.pipe(concat('scripts.min.js'))
 		// .pipe(uglify()) // Mifify js (opt.)
 		.pipe(gulp.dest('src/js'))
 		.pipe(browsersync.reload({ stream: true }))
--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
-  
+  $(".menu-btn-toggle").click(function (e) { 
+    $(".navigation__wrapper").toggleClass("hidden");
+    $("#icon-mobile-menu").toggleClass("fa-bars fa-times")
+  });
+});
+
+function AjaxFormRequest(result_id, formMain, url) {
+  jQuery.ajax({
+    url: url,
+    type: "POST",
+    dataType: "html",
+    data: jQuery("#" + formMain).serialize(),
+    success: function (response) {
+      $(':input', '#' + formMain)
+        .not(':button, :submit, :reset, :hidden')
+        .val('')
+        .removeAttr('checked')
+        .removeAttr('selected');
+      setTimeout(() => {
+        $("#message").hide();
+      }, 5000);
+    },
+    error: function (response) {
+      var par = document.getElementById(result_id);
+      var error = document.createElement('p');
+      error.classList.add("mt-3");
+      error.innerHTML = "Возникла ошибка при отправке формы.";
+      par.appendChild(error);
+      setTimeout(func, 700);
+    }
+  });
+}
+
+function func() {
+  $("p.mt-3").detach();
+}
+$('#feedback-form').submit(function (e) {
+  e.preventDefault();
+  AjaxFormRequest('result', 'feedback-form', '../feedback.php');
 });
